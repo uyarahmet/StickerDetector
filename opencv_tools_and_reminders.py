fd3476ed48eafbae1863048513f0b4ae2ea95f8b
Ahmet Uyar <auyar19@ku.edu.tr>
2022-02-05 20:27:57 +0300

    added Chapter 14

--- opencv_tools_and_reminders.py
+++ opencv_tools_and_reminders.py
@@ -87,11 +87,27 @@ CHAPTER #3 END ***************************
 
 ''' Chapter 4 Start
 
+****************EXAMPLE 1*************
 
+img = np.zeros((512,512,3), np.uint8)
+print(img)
+cv2.imshow("Image", img)
+cv2.waitkey(0)
 
+****************EXAMPLE 2*************
 
+img = np.zeros((512,512,3), np.uint8)
+img[:] = 255, 0, 0 # blue
+cv2.imshow("Image", img) # full blue 
+img[200:300, 100:300] = 255, 0, 0 # height, width
 
+***********EXAMPLE 3 IMPORTANT! ******
 
+cv2.line(img, (0,0), (300,300), (0, 255, 0), 3) # target, start, end, color, thickness
+cv2.line(img, (0,0), (img.shape[1], img.shape[0]), (0, 255, 0), 3) # target, start, end, color, thickness
+
+cv2.circle(img, (400,50), 30, (255, 255, 0), 5) # IMPORTANT!
+cv2.text
 
 Chapter 4 End
 '''
