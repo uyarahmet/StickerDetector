30c02da78c2fded8b936da9afbd01cf76299cf7b
Ahmet Uyar <auyar19@ku.edu.tr>
2022-02-06 13:17:13 +0300

    28 samples' centers of stickers are found out of 53

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import cv2
 import numpy as np
+import os
 
-# Read image.
-img = cv2.imread('Resources/IMG_0049.jpeg', cv2.IMREAD_COLOR)
-print(img.shape)
+for i in range(49,103): # from IMG_0049 to IMG_102
 
-# Convert to grayscale.
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.imread('Resources/IMG_00' + str(i) +'.jpeg', cv2.IMREAD_COLOR) # Reading image
+    # Convert to grayscale.
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Blur using 3 * 3 kernel.
+    gray_blurred = cv2.blur(gray, (3, 3))
+    # Apply Hough transform on the blurred image.
+    detected_circles = cv2.HoughCircles(gray_blurred,
+                                        cv2.HOUGH_GRADIENT, 1, 20, minRadius=50, maxRadius=400) # Existing logic
 
-# Blur using 3 * 3 kernel.
-gray_blurred = cv2.blur(gray, (3, 3))
+    # Draw circles that are detected.
+    if detected_circles is not None:
+        # Convert the circle parameters a, b and r to integers.
+        detected_circles = np.uint16(np.around(detected_circles))
 
-# Apply Hough transform on the blurred image.
-detected_circles = cv2.HoughCircles(gray_blurred,
-                                    cv2.HOUGH_GRADIENT, 1, 20, minRadius=200, maxRadius=2000)
+        for pt in detected_circles[0, :]: # X,Y coordinates of the center with radius
+            a, b, r = pt[0], pt[1], pt[2]
+            print(f'X = {a}, Y = {b}, Radius = {r}') # assign x, y, r
+            # Draw the circumference of the circle.
+            cv2.circle(img, (a, b), r, (0, 255, 0), 10)
+            # Draw a small circle (of radius 1) to show the center (needed for later).
+            cv2.circle(img, (a, b), 1, (0, 0, 255), 30)
+        path = 'Output/'
+        cv2.imwrite(os.path.join(path, 'img' + str(i) + '.jpeg'), img)
 
-# Draw circles that are detected.
-if detected_circles is not None:
-    print("detected 1")
-    # Convert the circle parameters a, b and r to integers.
-    detected_circles = np.uint16(np.around(detected_circles))
-
-    for pt in detected_circles[0, :]:
-        a, b, r = pt[0], pt[1], pt[2]
-
-        # Draw the circumference of the circle.
-        cv2.circle(img, (a, b), r, (0, 255, 0), 10)
-
-        # Draw a small circle (of radius 1) to show the center.
-        cv2.circle(img, (a, b), 1, (0, 0, 255), 30)
-        cv2.imshow("Detected Circle", img)
-        cv2.waitKey(0)
