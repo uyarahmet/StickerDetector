35d2f33f56916f7ef0f600ba391bc9114b47b4c2
Berke Derin Berktay <98432576+brdrbr@users.noreply.github.com>
2022-02-17 17:07:48 +0300

    final python commit with glob and the diameter label

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
 import os
+import glob
+files = glob.glob('Resources/*.jpeg',
+                   recursive = True)
+for file in files: # from IMG_0049 to IMG_102
 
-for i in range(49,103): # from IMG_0049 to IMG_102
-
-    if i >= 100:
-        img = cv2.imread('Resources/IMG_0' + str(i) + '.jpeg', cv2.IMREAD_COLOR)  # Reading image
-    else:
-        img = cv2.imread('Resources/IMG_00' + str(i) + '.jpeg', cv2.IMREAD_COLOR)  # Reading image
+    img = cv2.imread(file, cv2.IMREAD_COLOR)
 
     def empty(a):
         pass
@@ -29,7 +28,7 @@ for i in range(49,103): # from IMG_0049 to IMG_102
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
     mask = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow("Origin " + str(i), mask)
+    #cv2.imshow("Origin " + str(i), mask)
     cv2.waitKey(1)
 
     detected_circles = cv2.HoughCircles(mask,
@@ -38,7 +37,6 @@ for i in range(49,103): # from IMG_0049 to IMG_102
     if detected_circles is not None:
         # Convert the circle parameters a, b and r to integers.
         detected_circles = np.uint16(np.around(detected_circles))
-
         for pt in detected_circles[0, :]: # X,Y coordinates of the center with radius
             a, b, r = pt[0], pt[1], pt[2]
             print(f'X = {a}, Y = {b}, Radius = {r}') # assign x, y, r
@@ -46,15 +44,10 @@ for i in range(49,103): # from IMG_0049 to IMG_102
             cv2.circle(img, (a, b), r, (0, 255, 0), 10)
             # Draw a small circle (of radius 1) to show the center (needed for later).
             cv2.circle(img, (a, b), 1, (0, 0, 255), 30)
+        x, y, w, h = 0, 0, 500, 250
+        # Draw black background rectangle
+        cv2.rectangle(img, (x, x), (x + w, y + h), (0, 0, 0), -1)
+        cv2.putText(img, str(r*2), (x + int(w / 10), 200), cv2.FONT_HERSHEY_SIMPLEX, 5,
+                    (255, 255, 255), 10)
         path = 'Output/'
-        cv2.imwrite(os.path.join(path, 'IMG_' + str(i) + '.jpeg'), img)
-
-
-
-
-
-
-
-
-
-
+        cv2.imwrite(os.path.join(path, str(file[10:23])), img)
