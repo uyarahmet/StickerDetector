91bdce149b14e86344e46e2402b57a2148a7dfa3
Berke Derin Berktay <98432576+brdrbr@users.noreply.github.com>
2022-02-19 00:06:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,19 @@
 #  email: auyar19@ku.edu.tr, bberktay19@ku.edu.tr, oguler@ekareinc.com
 #
 # ---------------------------------------------------------------------------
+import cv2
+import numpy as np
+import os
 from detector import *
 import glob
-
-def main():
-    files = glob.glob('Resources/*.jpeg',
-                       recursive = True)
-    for file in files: # from IMG_0049 to IMG_102
-        detector_method(file)
-
-if __name__ == '__main__':
-    main()
+files = glob.glob('Resources/*.jpeg',
+                   recursive = True)
+for file in files:
+    final = detectormethod(file)
+    if(final[0] == 1):
+        img = cv2.imread(file, cv2.IMREAD_COLOR)
+        cv2.circle(img, (int(final[3] + final[1]/2), int(final[4] + final[1]/2)), int(final[1]/2), (0, 255, 0), 10)
+        cv2.circle(img, (int(final[3] + final[1]/2), int(final[4] + final[1]/2)), 1, (0, 0, 255), 30)
+        cv2.rectangle(img, (final[3],final[4]), (final[3] + final[2], final[4] + final[1]), (0, 0, 255),10)
+        path = 'Output/'
+        cv2.imwrite(os.path.join(path, str(file[10:23])), img)
