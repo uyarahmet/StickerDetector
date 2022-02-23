f33cda5856ace9fbadea7ca2e0735621876e7ad5
Ahmet Uyar <auyar19@ku.edu.tr>
2022-02-23 16:54:15 -0500

    enhanced main

--- main.py
+++ main.py
@@ -15,13 +15,9 @@ files = glob.glob('Resources/*.jpeg',
                    recursive = True)
 for file in files:
     unchanged_img = cv2.imread(file, cv2.IMREAD_COLOR)
-    width = 1000
-    height = (width * unchanged_img.shape[0])/unchanged_img.shape[1] # (w2 * h1 / w1) = h2
-    dsize = (width, int(height))
-    img = cv2.resize(unchanged_img, dsize)
-    final = detectormethod(img)
+    final = detectormethod(unchanged_img)
     if(final[0] == 1):
-        new_img = img
+        new_img = final[5]
         cv2.circle(new_img, (int(final[3] + final[1]/2), int(final[4] + final[1]/2)), int(final[1]/2), (0, 255, 0), 10)
         cv2.circle(new_img, (int(final[3] + final[1]/2), int(final[4] + final[1]/2)), 1, (0, 0, 255), 30)
         cv2.rectangle(new_img, (final[3],final[4]), (final[3] + final[2], final[4] + final[1]), (0, 0, 255),10)
